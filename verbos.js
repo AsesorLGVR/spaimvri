3634d5124d17a311f29a9523d22bbb4d49f8bb1d
SpanishImmersive <68026156+AsesorLGVR@users.noreply.github.com>
2021-04-03 11:27:18 +0200

    Update verbos.js

--- verbos.js
+++ verbos.js
@@ -1,10 +1,3 @@
-$(document).ready(function(){
- 
-    $('#intro').css({'display': 'block','opacity': '1'});
-    $('#intro').animate({'opacity': '0','top':'-500px'},8000);
-	});
-  
-
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
